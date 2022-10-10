e731f1721258b189169056071c18c1e4264770cc
BKSalman <kfoo2023@gmail.com>
2022-10-10 03:35:11 +0300

    basic download, download playlists, choose format

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::{path::{Path, PathBuf}, process::Command};
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use iced::{
-    button, text_input::State, window, Button, Checkbox, Color, Column, Container, Element, Length,
-    Radio, Row, Sandbox, Settings, Text, TextInput,
+    button,
+    text_input::{self, State},
+    window, Background, Button, Checkbox, Color, Column, Container, Element, Length, Radio, Row,
+    Sandbox, Settings, Text, TextInput,
 };
 
 use native_dialog::FileDialog;
@@ -15,11 +20,13 @@ pub enum Message {
     TogglePlaylist(bool),
     SelectedVideoFormat(VideoFormat),
     SelectFolder,
+    SelectFolderTextInput(String),
 }
 
-#[derive(Default)]
+// #[derive(Default)]
 struct YtGUI {
     link_state: State,
+    download_folder_state: State,
     download_button_state: button::State,
     dialog_button_state: button::State,
     download_link: String,
@@ -27,6 +34,56 @@ struct YtGUI {
     is_playlist: bool,
     format: VideoFormat,
     download_folder: Option<PathBuf>,
+    placeholder: String,
+}
+
+impl Default for YtGUI {
+    fn default() -> Self {
+        Self {
+            download_folder: Some(PathBuf::from("~/Videos")),
+            link_state: State::default(),
+            download_folder_state: State::default(),
+            download_button_state: button::State::default(),
+            dialog_button_state: button::State::default(),
+            download_link: String::default(),
+            resolution: Resolution::default(),
+            is_playlist: bool::default(),
+            format: VideoFormat::default(),
+            placeholder: "Download link".to_string(),
+        }
+    }
+}
+
+pub struct TextInputStyles;
+
+impl text_input::StyleSheet for TextInputStyles {
+    fn active(&self) -> text_input::Style {
+        text_input::Style {
+            background: Background::Color(Color::WHITE),
+            border_radius: 2.0,
+            border_width: 1.0,
+            border_color: Color::from_rgb(0.7, 0.7, 0.7),
+        }
+    }
+
+    fn focused(&self) -> text_input::Style {
+        text_input::Style {
+            border_color: Color::from_rgb(0.5, 0.5, 0.5),
+            ..self.active()
+        }
+    }
+
+    fn placeholder_color(&self) -> Color {
+        Color::from_rgb(0.7, 0.7, 0.7)
+    }
+
+    fn value_color(&self) -> Color {
+        Color::from_rgb(0.3, 0.3, 0.3)
+    }
+
+    fn selection_color(&self) -> Color {
+        Color::from_rgb(0.8, 0.8, 1.0)
+    }
 }
 
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
@@ -59,23 +116,79 @@ impl Sandbox for YtGUI {
     }
 
     fn update(&mut self, event: Message) {
+        if !self.download_folder.clone().unwrap().is_dir()
+            && !self.download_folder_state.is_focused()
+        {
+            self.download_folder = Some(PathBuf::from("~/Videos"));
+        }
+
         match event {
             Message::Download(link) => {
-                let output_path = Path::new("~/Videos");
+                if self.download_link.is_empty() {
+                    self.placeholder = "No Download link was provided!".to_string();
+                    return;
+                }
+                self.placeholder = "Download link".to_string();
 
-                Command::new("./yt-dlp")
-                    .args([
+                let mut command = Command::new("yt-dlp");
+                if self.is_playlist {
+                    command.args([
                         &link,
                         "-o",
                         &format!(
                             "{}/{}",
-                            output_path.to_str().expect("No Videos Directory"),
+                            self.download_folder
+                                .clone()
+                                .unwrap()
+                                .to_str()
+                                .expect("No Videos Directory"),
+                            "%(uploader)s/%(playlist)s - %(title)s.%(ext)s"
+                        ),
+                    ]);
+                } else {
+                    command.args([
+                        &link,
+                        "-o",
+                        &format!(
+                            "{}/{}",
+                            self.download_folder
+                                .clone()
+                                .unwrap()
+                                .to_str()
+                                .expect("No Videos Directory"),
                             "%(title)s.%(ext)s"
                         ),
-                    ])
-                    .spawn()
-                    .expect("failed to execute process");
-                println!("{link}");
+                    ]);
+                }
+                
+                match self.format {
+                    VideoFormat::Flv => {
+                        command.args([
+                            "--format",
+                            "flv"
+                        ]);
+                    }
+                    VideoFormat::Mp4 => {
+                        command.args([
+                            "--format",
+                            "mp4"
+                        ]);
+                    }
+                    VideoFormat::ThreeGP => {
+                        command.args([
+                            "--format",
+                            "3gp"
+                        ]);
+                    }
+                    VideoFormat::Webm => {
+                        command.args([
+                            "--format",
+                            "webm"
+                        ]);
+                    }
+                }
+                
+                command.spawn().expect("failed to execute process");
             }
             Message::InputChanged(input) => {
                 self.download_link = input;
@@ -90,13 +203,23 @@ impl Sandbox for YtGUI {
                 self.format = format;
             }
             Message::SelectFolder => {
-                self.download_folder = FileDialog::new()
-                    .set_location("~/Downloads")
+                if let Ok(Some(path)) = FileDialog::new()
+                    .set_location(
+                        self.download_folder
+                            .clone()
+                            .expect("download folder")
+                            .to_str()
+                            .expect("download folder as string"),
+                    )
                     .show_open_single_dir()
-                    .unwrap_or_else(|e| {
-                        println!("{e}");
-                        Some(PathBuf::from("~/Downloads"))
-                    });
+                {
+                    self.download_folder = Some(path);
+                }
+            }
+            Message::SelectFolderTextInput(folder_string) => {
+                let path = PathBuf::from(folder_string);
+
+                self.download_folder = Some(path);
             }
         }
     }
@@ -111,95 +234,136 @@ impl Sandbox for YtGUI {
                     .push(
                         TextInput::new(
                             &mut self.link_state,
-                            "Download link",
+                            &self.placeholder,
                             &self.download_link,
                             Message::InputChanged,
                         )
-                        .size(25),
+                        .style(TextInputStyles)
+                        .size(18)
+                        .width(Length::Fill),
                     )
                     .push(Checkbox::new(
                         self.is_playlist,
                         "Playlist",
                         Message::TogglePlaylist,
                     ))
-                    .spacing(12)
+                    .spacing(7)
                     .align_items(iced::Alignment::Center),
             )
             .push(
                 Row::new()
                     .push(Text::new("Resolution: "))
-                    .push(Radio::new(
-                        Resolution::FourK,
-                        "4K",
-                        Some(self.resolution),
-                        Message::SelectedResolution,
-                    ))
-                    .push(Radio::new(
-                        Resolution::TwoK,
-                        "1440p",
-                        Some(self.resolution),
-                        Message::SelectedResolution,
-                    ))
-                    .push(Radio::new(
-                        Resolution::FullHD,
-                        "1080p",
-                        Some(self.resolution),
-                        Message::SelectedResolution,
-                    ))
-                    .push(Radio::new(
-                        Resolution::Hd,
-                        "720p",
-                        Some(self.resolution),
-                        Message::SelectedResolution,
-                    ))
-                    .push(Radio::new(
-                        Resolution::Sd,
-                        "480p",
-                        Some(self.resolution),
-                        Message::SelectedResolution,
-                    ))
+                    .push(
+                        Radio::new(
+                            Resolution::FourK,
+                            "4K",
+                            Some(self.resolution),
+                            Message::SelectedResolution,
+                        )
+                        .size(19),
+                    )
+                    .push(
+                        Radio::new(
+                            Resolution::TwoK,
+                            "1440p",
+                            Some(self.resolution),
+                            Message::SelectedResolution,
+                        )
+                        .size(19),
+                    )
+                    .push(
+                        Radio::new(
+                            Resolution::FullHD,
+                            "1080p",
+                            Some(self.resolution),
+                            Message::SelectedResolution,
+                        )
+                        .size(19),
+                    )
+                    .push(
+                        Radio::new(
+                            Resolution::Hd,
+                            "720p",
+                            Some(self.resolution),
+                            Message::SelectedResolution,
+                        )
+                        .size(19),
+                    )
+                    .push(
+                        Radio::new(
+                            Resolution::Sd,
+                            "480p",
+                            Some(self.resolution),
+                            Message::SelectedResolution,
+                        )
+                        .size(19),
+                    )
                     .spacing(12)
                     .align_items(iced::Alignment::Center),
             )
             .push(
                 Row::new()
                     .push(Text::new("Preferred Format: "))
-                    .push(Radio::new(
-                        VideoFormat::Mp4,
-                        "MP4",
-                        Some(self.format),
-                        Message::SelectedVideoFormat,
-                    ))
-                    .push(Radio::new(
-                        VideoFormat::Webm,
-                        "WEBM",
-                        Some(self.format),
-                        Message::SelectedVideoFormat,
-                    ))
-                    .push(Radio::new(
-                        VideoFormat::ThreeGP,
-                        "3GP",
-                        Some(self.format),
-                        Message::SelectedVideoFormat,
-                    ))
-                    .push(Radio::new(
-                        VideoFormat::Flv,
-                        "FLV",
-                        Some(self.format),
-                        Message::SelectedVideoFormat,
-                    ))
+                    .push(
+                        Radio::new(
+                            VideoFormat::Mp4,
+                            "MP4",
+                            Some(self.format),
+                            Message::SelectedVideoFormat,
+                        )
+                        .size(19),
+                    )
+                    .push(
+                        Radio::new(
+                            VideoFormat::Webm,
+                            "WEBM",
+                            Some(self.format),
+                            Message::SelectedVideoFormat,
+                        )
+                        .size(19),
+                    )
+                    .push(
+                        Radio::new(
+                            VideoFormat::ThreeGP,
+                            "3GP",
+                            Some(self.format),
+                            Message::SelectedVideoFormat,
+                        )
+                        .size(19),
+                    )
+                    // .push(
+                    //     Radio::new(
+                    //         VideoFormat::Flv,
+                    //         "FLV",
+                    //         Some(self.format),
+                    //         Message::SelectedVideoFormat,
+                    //     )
+                    //     .size(19),
+                    // )
                     .spacing(12)
                     .align_items(iced::Alignment::Center),
             )
             .push(
-                Row::new().push(
-                    Button::new(&mut self.dialog_button_state, Text::new("Browse"))
-                        .on_press(Message::SelectFolder),
+                Row::new()
+                    .push(
+                        Button::new(&mut self.dialog_button_state, Text::new("Browse"))
+                            .on_press(Message::SelectFolder),
                     )
-                .push(
-                    Button::new(&mut self.download_button_state, Text::new("Download"))
-                        .on_press(Message::Download(self.download_link.clone())),
-                )
+                    .push(
+                        TextInput::new(
+                            &mut self.download_folder_state,
+                            "",
+                            self.download_folder.clone().unwrap().to_str().unwrap(),
+                            Message::SelectFolderTextInput,
+                        )
+                        .style(TextInputStyles),
+                    )
+                    .push(
+                        Button::new(&mut self.download_button_state, Text::new("Download"))
+                            .on_press(Message::Download(self.download_link.clone())),
+                    )
+                    .spacing(12)
+                    .align_items(iced::Alignment::Center),
             )
             .width(Length::Fill)
             .align_items(iced::Alignment::Fill)
