b682d2b16f133faaaa5a41418c6e4b5a59cce3c5
Salman <salman.f.abuhaimed@gmail.com>
2023-07-29 14:07:36 +0300

    add flake package + remove unused deps from Cargo.toml

--- flake.nix
+++ flake.nix
@@ -4,18 +4,24 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    crane,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        craneLib = crane.lib.${system};
         rustOverlay = builtins.fetchTarball {
           url = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
-          sha256 = "1cr3ph1ww4scgw3hdhnag2qpqx36xplvlsjwa3z6rmrf424zqx9z";
+          sha256 = "1jcfh1n57sq3g1mxdf6grqc0rcpams14gbampshfvx0g459b2sj9";
         };
         pkgs = import nixpkgs { inherit system; overlays = [ (import rustOverlay) ]; };
         libPath =  with pkgs; lib.makeLibraryPath [
@@ -28,37 +34,59 @@
           xorg.libXrandr
           xorg.libXi
         ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          cmake
+          mesa
+          makeWrapper
+        ];
+
+        buildInputs = with pkgs; [
+          fontconfig
+          freetype
+
+          vulkan-headers
+          vulkan-loader
+          libGL
+
+          libxkbcommon
+          # WINIT_UNIX_BACKEND=wayland
+          wayland
+
+          # WINIT_UNIX_BACKEND=x11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libX11
+        ];
       in with pkgs; {
-        devShell = mkShell rec {
-          packages = [
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          inherit buildInputs nativeBuildInputs;
+
+          # idk how to make this work
+          # postInstall = ''
+          #   for _size in "16x16" "32x32" "48x48" "64x64" "128x128" "256x256"; do
+          #       install -Dm644 "$src/data/icons/$_size/apps/ytdlp-gui.png" "$out/share/icons/hicolor/$_size/apps/ytdlp-gui.png"
+          #   done
+          #   install -Dm644 "$src/data/applications/ytdlp-gui.desktop" -t "$out/share/applications/"
+          # '';
+
+          postInstall = ''
+            wrapProgram $out/bin/ytdlp-gui \
+              --prefix PATH : ${lib.makeBinPath [ pkgs.gnome.zenity pkgs.libsForQt5.kdialog]}\
+              --suffix LD_LIBRARY_PATH : ${libPath}
+          '';
+        };
+
+        devShell = mkShell {
+          inherit buildInputs nativeBuildInputs;
+
+          packages = with pkgs; [
             rust-bin.stable.latest.default
             rust-analyzer
-          ];
-          nativeBuildInputs = with pkgs; [
-            glibc
-            pkg-config
-            cmake
-            mesa
-            makeWrapper
-          ];
-
-          buildInputs = with pkgs; [
-            fontconfig
-            freetype
-
-            vulkan-headers
-            vulkan-loader
-            libGL
-
-            libxkbcommon
-            # WINIT_UNIX_BACKEND=wayland
-            wayland
-
-            # WINIT_UNIX_BACKEND=x11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libX11
           ];
           LD_LIBRARY_PATH = "${libPath}";
         };
