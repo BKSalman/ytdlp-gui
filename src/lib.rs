cc705ef20547d8deff13fb4f0b85f5317b71bd67
BKSalman <salman.f.abuhaimed@gmail.com>
2023-08-28 23:56:36 +0300

    reduce mantask deps
    reduce ytdlp-gui deps

--- src/lib.rs
+++ src/lib.rs
@@ -607,12 +607,11 @@ macro_rules! git_hash {
         match option_env!("GIT_HASH") {
             Some(hash) => hash.to_string(),
             None => {
-                let repo = gix::discover(std::env::current_dir().unwrap())
-                    .expect("current directory should be a git repository");
-                let rev = repo
-                    .rev_parse_single("HEAD")
-                    .expect("HEAD in the repository should have a revision id");
-                rev.to_string()
+                let output = std::process::Command::new("git")
+                    .args(["rev-parse", "HEAD"])
+                    .output()
+                    .unwrap();
+                String::from_utf8(output.stdout).unwrap()
             }
         }
     };
